fb65baffaec39209f84513d7258ad6e93fc14309
Vidya Pawar <52663320+vidyapawar106@users.noreply.github.com>
2022-01-12 13:01:43 +0000

    Update Solutions.sql

--- Solutions.sql
+++ Solutions.sql
@@ -155,6 +155,26 @@ where t.Request_at between '2013-10-01' and '2013-10-03'
 group by t.Request_at;
 
 
+# Solution Eighteen: Exchange Seats
+SELECT
+    (CASE
+        WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+        WHEN MOD(id, 2) != 0 AND counts = id THEN id
+        ELSE id - 1
+    END) AS id,
+    student
+FROM
+    seat,
+    (SELECT
+        COUNT(*) AS counts
+    FROM
+        seat) AS seat_counts
+ORDER BY id ASC;
+
+
+
+
+
 
 
 
